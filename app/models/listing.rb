84f971647353db4eb33d7bd05b69919f6219b6fd
EllieChen-Git <elliechen.etc@gmail.com>
2019-10-29 12:15:20 +1100

    added pages(routes, controller, views)

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :location
-  has_one_attached :pic
   has_many :questions, dependent: :destroy
+  has_one_attached :pic
   enum category: { face: 0, eyes: 1, lips: 2, accessories: 3 }  
 end

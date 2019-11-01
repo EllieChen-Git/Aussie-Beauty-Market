ea07018e8a8128fa8b2e72f8a7ffe33bd51eb3d3
EllieChen-Git <elliechen.etc@gmail.com>
2019-11-01 14:21:44 +1100

    fixed 'validates' issues in question model

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,8 +3,14 @@ class AnswersController < ApplicationController
         answer_params = params.require(:answer).permit(:body, :question_id)
         @answer = Answer.create(answer_params)
         if @answer.errors.any?
-            # render plain: "not working"
             render plain: "#{@answer.errors.messages} Please go back!" 
+
+            # @listing = Listing.find(params["answer"]["question_id"]["listing_id"])
+            # @suburb = @listing.location[:suburb]
+            # @postcode = @listing.location[:postcode]
+            # @state = @listing.location[:state]
+            # @question= Question.new
+            # render "listings/show"
         else
             redirect_to listing_path
         end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,12 @@ class QuestionsController < ApplicationController
         question_params = params.require(:question).permit(:body, :listing_id, :user_id)
         @question = Question.create(question_params)
         if @question.errors.any?
-            render plain: "#{@question.errors.messages}. Please go back!" 
+            @listing = Listing.find(params["question"]["listing_id"])
+            @suburb = @listing.location[:suburb]
+            @postcode = @listing.location[:postcode]
+            @state = @listing.location[:state]
+            @answer = Answer.new
+            render "listings/show"
         else
             redirect_to listing_path
         end

e89b6943df94c83660deaecee6ef2fd3f35451d3
EllieChen-Git <elliechen.etc@gmail.com>
2019-11-05 10:42:50 +1100

    fixed nav bar - dropdown (z-index)

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -37,31 +37,30 @@ class ListingsController < ApplicationController
 
     end
 
-def purchase
+    def purchase
+      # Stripe
+      session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        customer_email: current_user.email,
+        line_items: [{
+            name: @listing.title,
+            amount: @listing.price * 100,
+            currency: 'aud',
+            quantity: 1,
+        }],
+        payment_intent_data: {
+            metadata: {
+                user_id: current_user.id,
+                listing_id: @listing.id
+            }
+        },
+        success_url: "#{root_url}payments/success?userId=#{current_user.id}&listingId=#{@listing.id}",
+        cancel_url: "#{root_url}listings"
+    )
 
-  # Stripe
-  session = Stripe::Checkout::Session.create(
-    payment_method_types: ['card'],
-    customer_email: current_user.email,
-    line_items: [{
-        name: @listing.title,
-        amount: @listing.price * 100,
-        currency: 'aud',
-        quantity: 1,
-    }],
-    payment_intent_data: {
-        metadata: {
-            user_id: current_user.id,
-            listing_id: @listing.id
-        }
-    },
-    success_url: "#{root_url}payments/success?userId=#{current_user.id}&listingId=#{@listing.id}",
-    cancel_url: "#{root_url}listings"
-)
-
-@session_id = session.id
-@public_key = Rails.application.credentials.dig(:stripe, :public_key) 
-end
+    @session_id = session.id
+    @public_key = Rails.application.credentials.dig(:stripe, :public_key) 
+    end
 
 
     def edit
